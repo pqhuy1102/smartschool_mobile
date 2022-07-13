e2b8545f0b3ca82e72a76e7af2c7f74910ce2f48
son.cao <son.cao@executionlab.asia>
2022-07-13 11:42:31 +0700

    update ios env

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import UIKit
 import Flutter
-import Firebase
-import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -12,13 +10,4 @@ import FirebaseMessaging
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
-  override func application(_ application: UIApplication,
-  didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-
-   Messaging.messaging().apnsToken = deviceToken
-   print("Token: \(deviceToken)")
-   super.application(application,
-   didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
- }
 }
